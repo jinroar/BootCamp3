5da6116a70b131f2d99687c12e6586da07a2a0ed
jenecorpuz <jenerose.corpuz@trajector.com>
2024-09-11 20:35:21 +0800

    NPM

--- NOde/readfile.js
+++ NOde/readfile.js
@@ -4,3 +4,17 @@ const {readFile, readFileSync} = require ('fs'); //sync === blocking
 const txt = readFileSync('./hello.txt','utf8')
 
 console.log(txt)
+
+console.log('me first')
+
+//Callback
+//refactoring via callback (text,utf,(error object, variable)=>{function block})
+readFile('./hello.txt','utf8',(err, txt)=>{
+  console.log('me second '+txt)  
+});
+
+// //Promises
+// const { readFile2 } = require ('fs').promises;
+// //top leve await ayeee node 14
+// const file = await readFile2 ('./hello.txt', 'utf8');
+// console.log(file)
---
+++ NPM/Pokemon Package/poke.js
@@ -0,0 +1,5 @@
+import pokemon from 'pokemon';
+
+    for(let i = 1;i<=500;i++)
+    console.log( `Pokemon # ${i}: ${pokemon.getName(i)} `)
+   
