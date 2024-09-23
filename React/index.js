8324f87c388dd9a737bf06dfce5c6d091247b464
jenecorpuz <jenerose.corpuz@trajector.com>
2024-09-23 21:19:44 +0800

    Added React

---
+++ React/index.js
@@ -0,0 +1,11 @@
+//Start of React native CLI
+import { AppRegistry } from 'react-native'
+import App from './App'
+import { name as appName } from './app.json'
+
+AppRegistry.registerComponent(appName, () => App)
+
+
+//This allows you to optionally have multiple React Native root components,
+// which you might embed into different native screens.
+// End of React native CLI 
